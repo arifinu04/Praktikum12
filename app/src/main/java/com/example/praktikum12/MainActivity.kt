6df9b509690765203d96017041e3acbf003792a2
Arifinu <arifindriyanto0409@gmail.com>
2023-08-11 15:39:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum12/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.praktikum12.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
